b8121dd7d3976c62fc6fd649e37f64361dea9e3f
Amber Farrington and Karen Freeman-Smith <amber+karen@>
2016-10-10 11:54:46 -0700

    save progress before lunch

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,76 @@
 //UI
 $(document).ready (function() {
   var thisTurn = 1;
+  var guess = 0;
+  var guesses = 0;
   var game = new Simon(8);
+  var turnArray[];
+
   $('#start').submit(function() {
     Simon.initialize();
+    takeTurn();
     event.preventDefault();
   });
   //start turn
-  //show sequence from current array
+  function takeTurn() {
+    turnArray[]=Simon.turn(thisTurn);
+    //show sequence from current array
+    for(var i=0; i<turnArray.length(); i++){
+      if(turnArray[i]==1) {
+        //show bright red
+      } else if (turnArray[i]==2) {
+        //show bright yellow
+      } else if (turnArray[i]==3) {
+        //show bright green
+      } else if (turnArray[i]==4) {
+        //show bright blue
+      } else {
+        //show error - should never get here
+        console.log("invalid number in array");
+      }
+    }
+  }
+
+
   //listen for clicks until number == array length
-  //next turn
+  $('.red').onClick(function(){
+    guess=1;
+    guesses++;
+    if(guesses==thisTurn) {
+      thisTurn++;
+      //get next turn
+    }
+  });
+  $('.yellow').onClick(function(){
+    guess=2;
+    guesses++;
+    if(guesses==thisTurn) {
+      thisTurn++;
+      //get next turn
+    }
+  });
+  $('.green').onClick(function(){
+    guess=3;
+    guesses++;
+    if(guesses==thisTurn) {
+      thisTurn++;
+      //get next turn
+    }
+  });
+  $('.blue').onClick(function(){
+    guess=4;
+    guesses++;
+    if(guesses==thisTurn) {
+      thisTurn++;
+      //get next turn
+    }
+  });
+
+
   // --OR--
   //click is wrong color
   //end of game
-  $('.red').onClick(function(){
 
-  });
-  $('.yellow').onClick(function(){
-
-  });
-  $('.green').onClick(function(){
-
-  });
-  $('.blue').onClick(function(){
-
-  });
 });
 
 //Logic
