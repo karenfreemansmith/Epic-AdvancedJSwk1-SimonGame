d8c7e8b20606b265b4fa40f59a50da9050c32caf
Amber Farrington and Karen Freeman-Smith <amber+karen@>
2016-10-10 18:05:23 -0700

    try to get color change on click - fails

--- build/js/app.js
+++ build/js/app.js
@@ -4,7 +4,7 @@ var Simon = require('./../js/simone.js').game;
 //UI
 $(document).ready (function() {
   var thisTurn = 1;
-  var totalTurns = 8;
+  var totalTurns = 3;
   var guess = 0;
   var guesses = 0;
   var game = new Simon(totalTurns);
@@ -16,48 +16,36 @@ $(document).ready (function() {
     $('button#startbutton').attr("disabled", "enabled");
     event.preventDefault();
   });
-  //start turn
   function takeTurn() {
     guess=0;
     guesses = 0;
-    if(thisTurn>totalTurns) {
-      //end game
-      $('button#startbutton').removeAttr("disabled");
-    }
     turnArray=game.thisTurn(thisTurn);
-    //show sequence from current array
     var i=0;
     myVar=setInterval(function (){
-
         if(turnArray[i]==1) {
           $('div.1').css("background", "#FD1704");
           window.setTimeout(function() {
   					$('div.1').css("background", "#9E0000");
   				}, 300);
-
         } else if (turnArray[i]==2) {
           $('div.2').css("background", "yellow");
           window.setTimeout(function() {
             $('div.2').css("background", "goldenrod");
           }, 300);
-
         } else if (turnArray[i]==3) {
           $('div.3').css("background", "#67F704");
           window.setTimeout(function() {
             $('div.3').css("background", "green");
           }, 300);
-
         } else if (turnArray[i]==4) {
           $('div.4').css("background", "#99d6ff");
           window.setTimeout(function() {
             $('div.4').css("background", "blue");
           }, 300);
-
         } else {
           //show error - should never get here
           console.log("invalid number in array");
         }
-
       i++;
       if(i>=turnArray.length) {
         clearInterval(myVar);
@@ -65,13 +53,12 @@ $(document).ready (function() {
     }, 500);
   }
 
-
-  //listen for clicks until number == array length
   $('div.1').click(function(){
-
     guess=1;
+    window.setTimeout(function() {
+      $('div.1').css("background", "#9E0000");
+    }, 300);
     if(turnArray[guesses]==guess) {
-      // alert("Correct :)");
     } else {
       endGame();
     }
@@ -83,8 +70,10 @@ $(document).ready (function() {
   });
   $('div.2').click(function(){
     guess=2;
+    window.setTimeout(function() {
+      $('div.2').css("background", "goldenrod");
+    }, 300);
     if(turnArray[guesses]==guess) {
-      //alert("Correct :)");
     } else {
       endGame();
     }
@@ -96,8 +85,10 @@ $(document).ready (function() {
   });
   $('div.3').click(function(){
     guess=3;
+    window.setTimeout(function() {
+      $('div.3').css("background", "green");
+    }, 300);
     if(turnArray[guesses]==guess) {
-      //alert("Correct :)");
     } else {
       endGame();
     }
@@ -109,8 +100,10 @@ $(document).ready (function() {
   });
   $('div.4').click(function(){
     guess=4;
+    window.setTimeout(function() {
+      $('div.4').css("background", "blue");
+    }, 300);
     if(turnArray[guesses]==guess) {
-      //alert("Correct :)");
     } else {
       endGame();
     }
@@ -120,12 +113,20 @@ $(document).ready (function() {
       takeTurn();
     }
   });
-});
 
-function endGame() {
-  alert("WRONG...YOU LOSE!!!! Better go to art school and learn your colors!");
-  location.reload();
-}
+  if(thisTurn>=totalTurns) {
+    endGame();
+  }
+
+  function endGame() {
+    if(thisTurn>=totalTurns) {
+      alert("CONGRATULATIONS, YOU WIN!!!");
+    } else {
+      alert("WRONG...YOU LOSE!!!! Better go to art school and learn your colors!");
+    }
+    location.reload();
+  }
+});
 
 },{"./../js/simone.js":2}],2:[function(require,module,exports){
 //Logic
--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var Simon = require('./../js/simone.js').game;
 //UI
 $(document).ready (function() {
   var thisTurn = 1;
-  var totalTurns = 8;
+  var totalTurns = 3;
   var guess = 0;
   var guesses = 0;
   var game = new Simon(totalTurns);
@@ -15,48 +15,36 @@ $(document).ready (function() {
     $('button#startbutton').attr("disabled", "enabled");
     event.preventDefault();
   });
-  //start turn
   function takeTurn() {
     guess=0;
     guesses = 0;
-    if(thisTurn>totalTurns) {
-      //end game
-      $('button#startbutton').removeAttr("disabled");
-    }
     turnArray=game.thisTurn(thisTurn);
-    //show sequence from current array
     var i=0;
     myVar=setInterval(function (){
-
         if(turnArray[i]==1) {
           $('div.1').css("background", "#FD1704");
           window.setTimeout(function() {
   					$('div.1').css("background", "#9E0000");
   				}, 300);
-
         } else if (turnArray[i]==2) {
           $('div.2').css("background", "yellow");
           window.setTimeout(function() {
             $('div.2').css("background", "goldenrod");
           }, 300);
-
         } else if (turnArray[i]==3) {
           $('div.3').css("background", "#67F704");
           window.setTimeout(function() {
             $('div.3').css("background", "green");
           }, 300);
-
         } else if (turnArray[i]==4) {
           $('div.4').css("background", "#99d6ff");
           window.setTimeout(function() {
             $('div.4').css("background", "blue");
           }, 300);
-
         } else {
           //show error - should never get here
           console.log("invalid number in array");
         }
-
       i++;
       if(i>=turnArray.length) {
         clearInterval(myVar);
@@ -64,13 +52,12 @@ $(document).ready (function() {
     }, 500);
   }
 
-
-  //listen for clicks until number == array length
   $('div.1').click(function(){
-
     guess=1;
+    window.setTimeout(function() {
+      $('div.1').css("background", "#9E0000");
+    }, 300);
     if(turnArray[guesses]==guess) {
-      // alert("Correct :)");
     } else {
       endGame();
     }
@@ -82,8 +69,10 @@ $(document).ready (function() {
   });
   $('div.2').click(function(){
     guess=2;
+    window.setTimeout(function() {
+      $('div.2').css("background", "goldenrod");
+    }, 300);
     if(turnArray[guesses]==guess) {
-      //alert("Correct :)");
     } else {
       endGame();
     }
@@ -95,8 +84,10 @@ $(document).ready (function() {
   });
   $('div.3').click(function(){
     guess=3;
+    window.setTimeout(function() {
+      $('div.3').css("background", "green");
+    }, 300);
     if(turnArray[guesses]==guess) {
-      //alert("Correct :)");
     } else {
       endGame();
     }
@@ -108,8 +99,10 @@ $(document).ready (function() {
   });
   $('div.4').click(function(){
     guess=4;
+    window.setTimeout(function() {
+      $('div.4').css("background", "blue");
+    }, 300);
     if(turnArray[guesses]==guess) {
-      //alert("Correct :)");
     } else {
       endGame();
     }
@@ -119,9 +112,17 @@ $(document).ready (function() {
       takeTurn();
     }
   });
-});
 
-function endGame() {
-  alert("WRONG...YOU LOSE!!!! Better go to art school and learn your colors!");
-  location.reload();
-}
+  if(thisTurn>=totalTurns) {
+    endGame();
+  }
+
+  function endGame() {
+    if(thisTurn>=totalTurns) {
+      alert("CONGRATULATIONS, YOU WIN!!!");
+    } else {
+      alert("WRONG...YOU LOSE!!!! Better go to art school and learn your colors!");
+    }
+    location.reload();
+  }
+});
